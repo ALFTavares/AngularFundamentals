07e2aab835cd685389d378ec7861171e92baffa9
mouse <alftavares1985@gmail.com>
2018-06-25 16:49:29 +0100

    added ngClass

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,8 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <div class="well hoverwell thumbnail">
         <h2>{{ event?.name }}</h2>
         <div>Date: {{ event?.date }}</div>
-        <div [ngSwitch]="event.time">Time: {{ event?.time }}
+        <div [ngClass]="getStartTimeClass()" 
+             [ngSwitch]="event.time">Time: {{ event?.time }}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
@@ -22,6 +23,8 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     </div>
     `,
     styles: [`
+        .green { color: #00FF00 !important; }
+        .bold { font-weight: bold; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
         .thumbnail { min-height: 210px; }
@@ -29,4 +32,10 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am')
+            return ['green', 'bold'];
+        return [];
+    }
 }
